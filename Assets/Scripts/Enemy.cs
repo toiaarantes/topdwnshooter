8480d94fb2a915a9d8de4227801794447b0580be
Vitória <vi.maar@hotmail.com>
2018-11-20 18:50:21 -0200

    no message

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,12 +7,18 @@ public class Enemy : MonoBehaviour {
     public float velocidade = 10;
     private Transform posJogador;
     private Player player;
+    public GameObject efeitoTiro;
+    public GameObject efeitoInimigo;
+    private ScreenShake shake;
 	// Use this for initialization
 	void Start () {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
         posJogador = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-	}
-	
+        shake = GameObject.FindGameObjectWithTag("ScnSke").GetComponent<ScreenShake>();
+
+
+
+    }
 	// Update is called once per frame
 	void Update () {
         transform.position = Vector2.MoveTowards(transform.position, posJogador.position, velocidade * Time.deltaTime);
@@ -24,10 +30,18 @@ public class Enemy : MonoBehaviour {
         {
             player.vida--;
             Debug.Log(player.vida);
+            GameObject ef = Instantiate(efeitoInimigo, transform.position, Quaternion.identity);
+            shake.CamShake();
+            Destroy(ef, 2f);
             Destroy(gameObject);
         }
         if(other.CompareTag("Projetil"))
         {
+            GameObject ef = Instantiate(efeitoInimigo, transform.position, Quaternion.identity);
+            GameObject efT = Instantiate(efeitoTiro, transform.position, Quaternion.identity);
+            shake.CamShake();
+            Destroy(ef, 2f);
+            Destroy(efT, 2f);
             Destroy(other.gameObject);
             Destroy(gameObject);
         }
---
+++ Assets/Scripts/ScreenShake.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenShake : MonoBehaviour {
+
+    public Animator cameraAnim;
+
+    public void CamShake()
+    {
+        cameraAnim.SetTrigger("shake");
+    }
+}
